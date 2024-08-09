552944d2af80de776a58f7336ed57d6ea2648c25
noobitak <131758035+noobitak@users.noreply.github.com>
2024-08-09 11:52:26 +0800

    MakeCode 项目的初始文件

---
+++ main.ts
@@ -0,0 +1,13 @@
+input.onButtonPressed(Button.A, function () {
+    music.play(music.createSoundExpression(WaveShape.Sine, 5000, 0, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
+})
+basic.showLeds(`
+    . . . . .
+    . # . # .
+    . . . . .
+    # . . . #
+    . # # # .
+    `)
+basic.forever(function () {
+    pins.servoWritePin(AnalogPin.P10, 180)
+})
